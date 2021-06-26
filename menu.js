137958be76873ab12ab438a7b212ebebb05cb5e4
Anderson Ribeiro Nunes <andersonribeironunes@hotmail.com>
2021-06-26 14:58:07 -0300

    estudo menu responsivo

---
+++ menu.js
@@ -0,0 +1,12 @@
+var show = true
+
+var menuSection = document.querySelector(".menu-section")
+var menuToggle = menuSection.querySelector(".menu-toggle")
+
+menuToggle.addEventListener("click", () => {
+
+  document.body.style.overflow = show ? "hidden" : "initial"
+
+  menuSection.classList.toggle("on", show)
+  show = !show
+})
