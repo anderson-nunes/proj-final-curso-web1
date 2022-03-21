689b4de96e81b588740e6a9d6be07f73b8ace0a0
Anderson Ribeiro Nunes <andersonribeironunes@hotmail.com>
2022-03-21 09:32:33 -0300

    inserindo modal ao projeto final

--- js/menu.js
+++ js/menu.js
@@ -9,3 +9,17 @@ menuToggle.addEventListener("click", () => {
   menuSection.classList.toggle("on", show);
   show = !show;
 });
+
+// Função para abrir modal //
+
+function acao() {
+  var modal = document.querySelector(".modal");
+
+  modal.style.display = "block";
+}
+
+function fechar() {
+  var modal = document.querySelector(".modal");
+
+  modal.style.display = "none";
+}
