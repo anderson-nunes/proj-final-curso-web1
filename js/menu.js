07fb8c1b0a21962893c8c15ba37e9a351e187207
Anderson Ribeiro Nunes <andersonribeironunes@hotmail.com>
2022-03-25 10:15:58 -0300

    organizando CSS

--- js/menu.js
+++ js/menu.js
@@ -23,21 +23,22 @@ function validarFormulario() {
     document.getElementById("fid-email").style.border = "solid 0.7px red";
     document.getElementById("mensagem-erro").innerHTML =
       "INFORME TODOS OS CAMPOS";
-    document.getElementById("mensagem-erro").style.color = "white";
+    document.getElementById("mensagem-erro").style.color = "red";
   } else if (nome == "" && email != "") {
     document.getElementById("fid-nome").style.border = "solid 0.7px red";
     document.getElementById("fid-email").style.border = "none";
     document.getElementById("mensagem-erro").innerHTML = "INFORME O CAMPO NOME";
-    document.getElementById("mensagem-erro").style.color = "white";
+    document.getElementById("mensagem-erro").style.color = "red";
   } else if (nome != "" && email == "") {
     document.getElementById("fid-nome").style.border = "none";
     document.getElementById("fid-email").style.border = "solid 0.7px red";
+    document.getElementById("mensagem-erro").style.color = "red";
     document.getElementById("mensagem-erro").innerHTML =
       "INFORME O CAMPO E-MAIL";
   } else {
     document.getElementById("fid-nome").style.border = "none";
     document.getElementById("fid-email").style.border = "none";
     document.getElementById("mensagem-erro").innerHTML = "DADOS ENVIADOS";
-    document.getElementById("mensagem-erro").style.color = "white";
+    document.getElementById("mensagem-erro").style.color = "Lime";
   }
 }
